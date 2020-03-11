f6363b0d0c93a16a741a8dc466d0ab0db1689b90
DaniC00 <57891168+DaniC00@users.noreply.github.com>
2020-03-11 10:51:34 +0100

    Update Fight.py

--- ProyectoLimpio/Fight.py
+++ ProyectoLimpio/Fight.py
@@ -2,7 +2,8 @@
 #<<<<<EN ESTE FICHERO ESTAN LAS FUNCIONES DE LOS MODOS DE JUEGO>>>>>
 
 def arcade():
-    print("Falta implementar")
+    print("\n-----------------------------\nGenerando Mazo Enemigo:\n-----------------------------")
+    Decks.crearMazoAliadoRan(1)
 
 def pvp():
     print("Falta implementar")
